7131c1d94d8e4e3438e53115489c653529e7d54a
MrAbas <abasov2016@list.ru>
2024-04-15 00:13:40 +0300

    исправил иконки

--- src/FormIcons/FormIcons.tsx
+++ src/FormIcons/FormIcons.tsx
@@ -1,14 +1,14 @@
-/* import iconTg from "./tg.svg";
+import iconTg from "./tg.svg";
 import iconInst from "./inst.svg";
-import iconX from "./x.svg"; */
+import iconTw from "./twitter.svg";
 import styles from "./FormIcons.module.scss";
 
 export default function FormIcons() {
   return (
     <div className={styles.formIcons}>
-      <img src="./tg.svg" alt="telegram" className={styles.formIcon} />
-      <img src="./inst.svg" alt="instagram" className={styles.formIcon} />
-      <img src="./x.svg" alt="twitter(x)" className={styles.formIcon} />
+      <img src={iconTg} alt="telegram" className={styles.formIcon} />
+      <img src={iconInst} alt="instagram" className={styles.formIcon} />
+      <img src={iconTw} alt="twitter(x)" className={styles.formIcon} />
     </div>
   );
 }
