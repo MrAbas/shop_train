0b7a7df408b684726bdb0c504726c743b50c7b02
MrAbas <abasov2016@list.ru>
2024-04-21 20:21:15 +0300

    подключил роутер

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { store } from "./store/store";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const router = createBrowserRouter([
+  {
+    path: "/loginPage",
+    element: <App />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 );
